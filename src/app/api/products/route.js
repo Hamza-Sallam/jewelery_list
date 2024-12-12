e04b2c74c38010b43e1544f3823f4e2e8a68ef60
Hamza Sallam <hamza.sallam.eng@outlook.com>
2024-12-12 15:04:57 +0300

    env var fixes

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -2,20 +2,20 @@ import products from "./products.json";
 import {NextResponse } from "next/server";
 
 
-// const fetchGoldPrice = async () => {
-//     const response = await fetch('https://www.goldapi.io/api/XAU/USD', {
-//       method: 'GET',
-//       headers: {
-//         'x-access-token': `${process.env.GOLD_PRICE_API}`, 
-//       },
-//     });
-//     if (!response.ok) {
-//       console.error('Failed to fetch gold price:', await response.text());
-//       throw new Error('Failed to fetch gold price');
-//     }
-//     const data = await response.json();
-//     return data.price_gram_24k; //pure gold price per gram
-//   };
+const fetchGoldPrice = async () => {
+    const response = await fetch('https://www.goldapi.io/api/XAU/USD', {
+      method: 'GET',
+      headers: {
+        'x-access-token': process.env.GOLD_PRICE_API, 
+      },
+    });
+    if (!response.ok) {
+      console.error('Failed to fetch gold price:', await response.text());
+      throw new Error('Failed to fetch gold price');
+    }
+    const data = await response.json();
+    return data.price_gram_24k; //pure gold price per gram
+  };
     export async function GET(request) {
     const { searchParams } = new URL(request.url); // Extract query parameters
     
@@ -26,10 +26,10 @@ import {NextResponse } from "next/server";
     const maxPopularity = parseFloat(searchParams.get("maxPopularity")) || 5; // Default to max score
   
     // Enrich the products with the calculated price and popularity
-    // const goldPrice= await fetchGoldPrice();
+    const goldPrice= await fetchGoldPrice();
     const enrichedProducts = products.map((product) => ({
       ...product,
-      price: parseFloat(((product.popularityScore + 1) * product.weight * 6).toFixed(2)),
+      price: parseFloat(((product.popularityScore + 1) * product.weight * goldPrice).toFixed(2)),
       popularity: parseFloat((product.popularityScore / 20).toFixed(1)), // Scale to 5
     }));
   
